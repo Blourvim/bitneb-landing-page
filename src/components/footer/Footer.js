9798242361707d5c90a5c698f74cc2346007f110
Dorian <silisyumtr@gmail.com>
2021-09-02 05:02:13 +0300

    give footer button margin

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,13 +7,13 @@ const Footer =(props)=>{
 
 return(
     <footer>
-        <Grid className='footer-content' container xs={11}md={8}>
+        <Grid className='footer-content' container xs={11}md={10}>
         <Grid item lg={3} md={3} sm={3} xs={12} className='grid-item' >
 
         <img className='footer-logo'src={image} alt={alt}/>
 
         </Grid>
-        <Grid item lg={5} md={4} sm={6} xs={12} align-content-xs-center className='grid-item'>
+        <Grid item lg={6} md={6} sm={6} xs={12}  className='grid-item'>
         <div className='footer-divider'></div>
 
 <div className='center'>
@@ -22,10 +22,11 @@ return(
             </div>       
 
         </Grid>
-        <Grid item  lg={3}  md={4} sm={12}xs={12} align-content-xs-center className='grid-item'>
+        <Grid item style={{margin:20}} lg={2}  md={3} sm={2}xs={12} marginTop='20px' className='grid-item'>
         <button className='footer-button center'>{button}</button>
         </Grid>
     </Grid>
+    <div className='blue-ribbon'></div>
 </footer>
 )
 

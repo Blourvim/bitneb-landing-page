f924e9f7a4f6b08058dcada43a6b83c165926bd5
Dorian <silisyumtr@gmail.com>
2021-09-02 04:25:10 +0300

    make laptop section responsive

--- src/components/laptopSection/LaptopSection.js
+++ src/components/laptopSection/LaptopSection.js
@@ -1,22 +1,27 @@
 import LaptopCard from './LaptopCard';
-import laptopCards from '../../content/texts/laptopCards'
-import './laptop-section.css'
+import laptopCards from '../../content/texts/laptopCards';
+import './laptop-section.css';
+import Grid from '@material-ui/core/Grid';
 const LaptopSection =()=>{
 
 return(
 <section className="laptops-section">
 <div className="laptops-divider"></div>
 <div  class='laptops'>
+    <Grid container>
 {
     laptopCards.map(item=>{
 
         return(
+            <Grid item key={item.alt}  md={3} sm ={6}xs={12}>
             <LaptopCard content={item}/>
+            </Grid>
         )
 
 
     })
 }
+</Grid>
 </div>
 
 </section>
